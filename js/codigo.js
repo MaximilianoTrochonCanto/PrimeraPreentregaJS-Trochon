fa90059e8afc2fc078f7ecd9d8ced0ea0af80466
Maximiliano Trochon Canto <maxitc8@hotmail.com>
2023-07-10 02:35:45 -0300

    tercer commit

--- js/codigo.js
+++ js/codigo.js
@@ -1,51 +1,61 @@
-const ganador = Math.floor(Math.random() * 100) + 1;
+let ganador = Math.floor(Math.random() * 100) + 1;
 let chances = 5;
 const refresca = "Si querés volver a jugar, refrescá la página.";
 
 
-function ingresarNumero() {
-    let numero = Number(document.querySelector("#numero").value)
-    
-    if(numero > 100 || numero < 1) 
-        alert("El número debe estar entre 1 y 100.")
-    else        
-    while (chances > 0) {
-        if (!consultarNumero(numero)) {
-            chances--;
-            if (chances > 0) alert("Incorrecto, te quedan " + (chances) + " chances. Pista: " + pistaNumero(numero, chances))
-        }
-        break;
-    }
-    if (consultarNumero(numero)) tuResultado("Felicidades, ganaste la partida! ");
-    else
-        if (chances == 0) tuResultado("Lo sentimos, perdiste la partida. ")
+function numeroIncorrecto(numero) {
+    document.querySelector("#div").classList.remove("diverror")
+    document.querySelector("#div").offsetWidth;
+    document.querySelector("#div").classList.add("diverror")
+    document.querySelector("#div").onanimationend = () => alert("Incorrecto, te quedan " + (chances) + " chances. Pista: " + pistaNumero(numero, chances));
+}
 
+
+
+
+
+
+function ingresarNumero() {
+    let numero = Number(document.querySelector("#numero").value);
+    if (numero > 100 || numero < 1)
+        alert("El número debe estar entre 1 y 100.")
+    else
+        while (chances > 0) {
+            if (!consultarNumero(numero)) {
+                chances--;
+                if (chances > 0) numeroIncorrecto(numero);
+            }
+            break;
+        }
+    if (consultarNumero(numero)) tuResultado('rgb(171, 250, 171)', "Felicidades, ganaste la partida! ");
+    else if (chances == 0) tuResultado('red', "Lo sentimos, perdiste la partida. El correcto erá: " + ganador + ". ");
 }
 
 function consultarNumero(numero) {
+    
     return (numero == ganador)
 }
 
 
 function pistaNumero(numero, chances) {
     let pista = "";
-    
-    
+
+
     if (chances == 4) {
         switch (true) {
             case ganador % 10 == 0:
                 pista = "El numero es múltiplo de 10 y también "
                 break;
-                case ganador % 5 == 0:
-                    pista = "El numero es múltiplo de 5 y también "
-                    break;
-                    case ganador % 2 == 0:
-                        pista = "El numero es par y también "
-                        break;
-                        case ganador % 2 != 0:
-                            pista = "El numero es impar y también "
-                            break;
-                        }
+            case ganador % 5 == 0:
+                pista = "El numero es múltiplo de 5 y también "
+                break;
+            case ganador % 2 == 0:
+                pista = "El numero es par y también "
+                break;
+            case ganador % 2 != 0:
+                pista = "El numero es impar y también "
+                break;
+        }
     } else {
         switch (true) {
             case ganador > 0 && ganador < 16:
@@ -54,27 +64,30 @@ function pistaNumero(numero, chances) {
             case ganador > 15 && ganador < 31:
                 pista = "El numero esta entre 16-30 y es "
                 break;
-                case ganador > 30 && ganador < 46:
-                    pista = "El numero esta entre 31-45 y es "
+            case ganador > 30 && ganador < 46:
+                pista = "El numero esta entre 31-45 y es "
                 break;
-                case ganador > 45 && ganador < 61:
+            case ganador > 45 && ganador < 61:
                 pista = "El numero esta entre 46-60 y es "
                 break;
             case ganador > 60 && ganador < 76:
                 pista = "El numero esta entre 61-75 y es "
                 break;
-                case ganador > 75 && ganador < 90:
-                    pista = "El numero esta entre 76-90 y es "
-                    break;
-                    default:
-                        pista = "El numero es mayor a 90 y es "
-                    }
-                }
+            case ganador > 75 && ganador < 90:
+                pista = "El numero esta entre 76-90 y es "
+                break;
+            default:
+                pista = "El numero es mayor a 90 y es "
+        }
+    }
+
+    
     if (numero > ganador) pista += "menor al número que pusiste."
     else pista += "mayor al número que pusiste."
     return pista;
 }
-function tuResultado(texto){
-    alert(texto + refresca);
+function tuResultado(color, texto) {
     document.querySelector("#boton").disabled = true;
+    document.querySelector("#body").style.background = color;
+    alert(texto + refresca);
 } 
